a30feb34432b2a2bef358870aa9f989bd42ab0d0
NachoDelfiner <delfinerignacio@gmail.com>
2023-07-06 14:21:41 -0300

    actualizacion form

--- app/formulario-de-contact.js
+++ app/formulario-de-contact.js
@@ -28,3 +28,26 @@ emailInput.addEventListener("blur", verificacionEmail);
 
 // LO SIGUIENTE ES COMPROBAR SI LOS MENSAJES DE ERROR
 // ESTAN ACTIVOS NO DEJAR ENVIAR EL FORMULARIO
+
+const formContainer = document.querySelector("#formContainer");
+const exitoMsj = document.querySelector("#exito");
+const $form = document.querySelector("#form");
+$form.addEventListener("submit", handleSubmit);
+
+async function handleSubmit(e) {
+  e.prevendetDefault();
+
+  const form = new FormData(this);
+  const response = await fetch(this.action, {
+    method: this.method,
+    body: form,
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  if (response.ok) {
+    this.reset();
+    formContainer.style.display = "none";
+    exitoMsj.style.display = "block";
+  }
+}
